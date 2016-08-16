5e8c109a3d4647f9652b4e7d751de65b6199bd6d
Tilman Kamp <tilmankamp@posteo.de>
2016-08-16 14:36:57 +0200

    Removing former artifacts during prereq install

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -21,12 +21,17 @@ if ! grep -q $NPMRCSTR $NPMRCFILE; then
    echo $NPMRCSTR >> $NPMRCFILE
 fi
 
+/etc/init.d/ntp stop
+ntpd -q -g
+/etc/init.d/ntp start
+
 # install the various packages and build tools we'll need
 apt-get update
 apt-get install -y python-dev autoconf automake libtool bison
 
 # download and compile a recent version of swig.
 # You can change the version number if there is a newer release
+rm -rf swig-*
 wget http://prdownloads.sourceforge.net/swig/swig-$swigv.tar.gz
 tar xf swig-$swigv.tar.gz
 cd swig-$swigv/
@@ -36,10 +41,10 @@ bash Tools/pcre-build.sh
 make -j 4
 make install
 cd ..
-rm -r swig-$swigv swig-$swigv.tar.gz
 
 # download and compile a recent version of cmake
 # You can change the version number if there is a newer release
+rm -rf cmake-*
 wget --no-check-certificate https://cmake.org/files/v$cmakeva/cmake-$cmakev.tar.gz
 tar xf cmake-$cmakev.tar.gz
 cd cmake-$cmakev
@@ -50,6 +55,7 @@ cd ..
 rm -r cmake-$cmakev cmake-$cmakev.tar.gz
 
 # Get Mozilla's version of sphinxbase and build it
+rm -rf sphinxbase
 git clone https://github.com/mozilla/sphinxbase.git
 cd sphinxbase
 ./autogen.sh
@@ -58,6 +64,7 @@ make install
 cd ..
 
 # Get Mozilla's version of pocketsphinx and build it
+rm -rf sphinxbase
 git clone https://github.com/mozilla/pocketsphinx.git
 cd pocketsphinx
 ./autogen.sh
@@ -73,6 +80,7 @@ echo "/usr/local/lib/" >> /etc/ld.so.conf
 ldconfig
 
 # We then setup sox
+rm -rf sox-*
 wget --no-check-certificate https://sourceforge.net/projects/sox/files/sox/$soxv/sox-$soxv.tar.gz/download -O sox-$soxv.tar.gz
 tar xf sox-$soxv.tar.gz
 cd sox-$soxv
@@ -80,16 +88,15 @@ cd sox-$soxv
 make -j 4 -s
 make install
 cd ..
-rm -rf sox-$soxv.tar.gz sox-$soxv
 
 # Install a recent version of node and npm.
 # See https://nodejs.org/en/download/ for latest stable i386 download link
+rm -rf node-*
 wget --no-check-certificate https://nodejs.org/dist/$nodev/node-$nodev-linux-armv7l.tar.xz
 tar xf node-$nodev-linux-armv7l.tar.xz
 cd node-$nodev-linux-armv7l/
 cp -R bin include lib share /usr/local/
 cd ..
-rm -r node-$nodev-linux-armv7l node-$nodev-linux-armv7l.tar.xz
 
 # Install node bindings for cmake
 npm install -g cmake-js
