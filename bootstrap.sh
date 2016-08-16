683dbca82e8ca80d78c65a609967278f83d5f386
Tilman Kamp <tilmankamp@posteo.de>
2016-08-16 13:08:38 +0200

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -e
+
+echo "deb http://mirror.netcologne.de/raspbian/raspbian/ jessie main contrib non-free rpi" > /etc/apt/sources.list
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y git
+
+cd /opt
+git clone https://github.com/mozilla/vaani.raspberrypi
+cd vaani.raspberrypi
+
+./install-prereqs.sh
+./setup.sh
